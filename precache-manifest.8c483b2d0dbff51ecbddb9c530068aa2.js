f1171192b4b04a7e4b890003b060a3843b69d178
Abimael Santos <juanabimael@gmail.com>
2020-10-13 08:01:18 -0500

    Updates

---
+++ precache-manifest.8c483b2d0dbff51ecbddb9c530068aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bc7f3771ae4f40efa568151262c485",
+    "url": "/agenda/index.html/index.html"
+  },
+  {
+    "revision": "84a4f53da959b1f59f2f",
+    "url": "/agenda/index.html/static/css/main.24c08afe.chunk.css"
+  },
+  {
+    "revision": "6cc18d5841f3f4e2223d",
+    "url": "/agenda/index.html/static/js/2.002743ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agenda/index.html/static/js/2.002743ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4f53da959b1f59f2f",
+    "url": "/agenda/index.html/static/js/main.38ea1a1d.chunk.js"
+  },
+  {
+    "revision": "b46fdfaf69495a3eed25",
+    "url": "/agenda/index.html/static/js/runtime-main.481bdf26.js"
+  }
+]);
